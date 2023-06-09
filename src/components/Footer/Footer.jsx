3a490410c67added879d4f61818d104dd969845f
sebas088 <sebastianbecerra088@gmail.com>
2023-06-09 23:07:45 +0200

    Maquetación header y footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,18 +7,13 @@ export const Footer = () => {
     <footer className='containerFooter'>
       <div>
         <img className='logo' src={logo} alt='logo'></img>
-        <div className='name'>NineDonkeyStore</div>
+        <div className='nameLogo'>NineDonkeyStore</div>
       </div>
-      <div>
-        <h3>Sobre Nosotros:
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur deleniti dicta, eos deserunt perspiciatis ut qui doloribus rerum ad, accusamus at quis nam repellendus fugit ipsum voluptate cumque modi reiciendis!</p>
-        </h3>
-      </div>
-      <div>
-        <h3>Contactanos:
-          <p>WhatsApp: 999999999</p>
-          <p></p>
-        </h3>
+      <div className='containerContacts'>
+        <h3>Contactanos:</h3>
+        <p>WhatsApp: +99 999999999</p>
+        <p>Instagram: @NineDonkey</p>
+        <p>TikTok: NineDonkeyShoes</p>
       </div>
     </footer>
     
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './NavBar.css';
+import logoHeader from '../../assets/images/logo.png';
 import { CartWidget } from '../CartWidget/CartWidget';
 import { Link } from 'react-router-dom';
 
@@ -9,15 +10,20 @@ export const NavBar = () => {
     //   <p>NineShoeStore</p>
     //   <p>Hombre</p>
     //   <p>Mujer</p>
-    //   <p>Contactanos</p>
+    //   <p>Contactanos</p>s
     //   <CartWidget />
     // </div>
     <div className='menu'>
-      <Link className='nameLogo' to="/">NineDonkeyStore</Link>
       <Link className='textNavbar containerNavbar' to="/productos/hombre">Hombre</Link>
       <Link className='textNavbar containerNavbar' to="/productos/mujer">Mujer</Link>
+      <Link className='nameLogo' to="/">
+        <div>
+          <img src={logoHeader} className='logoHeader'/>
+          <div>NineDonkeyStore</div>
+        </div>
+      </Link>
       <Link className='textNavbar containerNavbar' >Contactanos</Link>
-      <Link className='textNavbar' to="/cart"><CartWidget/></Link>
+      <Link className='textNavbar containerCart' to="/cart"><CartWidget/></Link>
     </div>
   )
 }
